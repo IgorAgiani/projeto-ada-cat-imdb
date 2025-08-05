0eebebfd51ec3531effa0abcbab431fc6a3d1ed9
Jhenny <jhenny.ticona.sis@gmail.com>
2025-08-04 21:54:08 -0300

    @jhennyticona | Cadastrar diretor,autor e filme

--- src/Menu.java
+++ src/Menu.java
@@ -1,9 +1,14 @@
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int opcao;
+        ArrayList<Diretor> diretores = new ArrayList<>();
+        ArrayList<Ator> atores = new ArrayList<>();
+        ArrayList<Filme> filmes = new ArrayList<>();
+
         while (true) {
             System.out.println("*****************************************************");
             System.out.println("                                                     ");
@@ -23,19 +28,47 @@ public class Menu {
             opcao = scanner.nextInt();
             switch (opcao) {
                 case 1:
-                    System.out.println("Digite nome do Diretor");
-                    String nomeDiretor= scanner.nextLine();
-                    //cadastrarDiretor(String nomeDiretor);
+                    System.out.println("----Cadastrar Diretor----");
+                    System.out.print("Nome do Diretor: ");
+                    String nomeDiretor = scanner.nextLine();
+                    Diretor novoDiretor = new Diretor(nomeDiretor);
+                    diretores.add(novoDiretor);
                     break;
                 case 2:
-                    System.out.println();
+                    System.out.println("----Cadastrar Ator----");
+                    System.out.println("Nome do Ator");
+                    String nomeAtor = scanner.nextLine();
+                    Ator novoAtor = new Ator(nomeAtor);
+                    atores.add(novoAtor);
                     break;
                 case 3:
+                    System.out.println("----Cadastrar Filme----");
                     System.out.println("Digite nome do Filme");
-                    String nomeFilme = scanner.nextLine();
-                    break;
+                    String tituloFilme = scanner.nextLine();
+                    System.out.println("Data de Lançamento (formato: AAAA-MM-DD):");
+                    String data = scanner.nextLine();
+                    LocalDate dataLancamento = LocalDate.parse(data);
+                    System.out.println("Descrição:");
+                    String descricao = scanner.nextLine();
+                    System.out.println("Escolha o diretor pelo índice:");
+                    for (int i = 0, i<diretores.size(), i++){
+                    System.out.println(i + "- " + diretores.get(i).getnome());
+                }
+                int indice = scanner.nextInt();
+                scanner.nextLine();
+                Diretor diretorEscolhido = diretores.get(indice);
+
+                Filme novoFilme = new Filme(tituloFilme, dataLancamento, descricao, diretorEscolhido);
+                break;
                 case 4:
-                    System.out.println();
+                    System.out.println("----Buscar Filme pelo nome----");
+                    System.out.println("Digite nome do filme: ");
+                    String buscarFilme= scanner.nextLine();
+                    for(Filme f: filmes){
+                        if(f.getNome().equalsIgnoreCase(buscarFilme)){
+                            return f;
+                        }
+                }
                     break;
                 default:
                     System.out.printf("");
