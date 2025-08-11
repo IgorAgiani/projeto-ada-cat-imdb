5782ba15f4dfb1b3fd7336e4dc37b597b7cdd224
Jhenny <jhenny.ticona.sis@gmail.com>
2025-08-11 19:37:39 -0300

    @jhennyticona | remoção de setters não usados, class Diretor

--- src/Diretor.java
+++ src/Diretor.java
@@ -1,18 +1,14 @@
 // classe Diretor herda os atibutos da classe Pessoa
-public class Diretor extends Pessoa{
+public class Diretor extends Pessoa {
     private double avaliacao;
-
-    public double getAvaliacao() {
-        return avaliacao;
-    }
-
-    public void setAvaliacao(double avaliacao) {
-        this.avaliacao = avaliacao;
-    }
 
     public Diretor(String nome, String miniBiografia, double avaliacao) {
         super(nome, miniBiografia);
         this.avaliacao = avaliacao;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
     }
 
     @Override
