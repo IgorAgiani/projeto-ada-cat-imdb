30ba2985ec3151c74561b780ade870b83d034c81
Jhenny <jhenny.ticona.sis@gmail.com>
2025-08-11 19:38:12 -0300

    @jhennyticona | remoção de setters não usados, class Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -12,25 +12,14 @@ public abstract class Pessoa {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-
     public String getMiniBiografia() {
         return miniBiografia;
     }
 
-    public void setMiniBiografia(String miniBiografia) {
-        this.miniBiografia = miniBiografia;
-    }
-
-
-    public void exibirDetalhes(){
+    public void exibirDetalhes() {
         System.out.println("Nome: " + this.getNome());
-        System.out.println("Mini biografia: " +  this.getMiniBiografia());
+        System.out.println("Mini biografia: " + this.getMiniBiografia());
     }
-
 }
 
 
